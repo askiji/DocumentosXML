3ca137808016e735ebaf22dd2ecd266c53bd8afe
askiji <askijipro@gmail.com>
2022-10-19 18:44:59 +0200

    vamos por el 2

--- src/Principal.java
+++ src/Principal.java
@@ -7,6 +7,35 @@ import java.io.*;
 
 public class Principal {
 	public static void main(String[] args) throws ParserConfigurationException, TransformerFactoryConfigurationError, TransformerException {
+//		ejercicio1();
+		ejercicio2();
+
+	}
+
+	private static void ejercicio2() {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse(new File("alumnos.xml"));
+			// Obtenemos la lista de nodos con el nombre empleado de todo el documento
+			NodeList empleados = doc.getElementsByTagName("alumno");
+			// Creamos un bucle para recorrer toda la lista de nodos
+			
+			//TODO ver el max
+			for (int i = 0; i < empleados.getLength(); i++) {
+				Node nodoEmple = empleados.item(i);
+				if (nodoEmple.getNodeType() == Node.ELEMENT_NODE) {
+					Element elem = (Element) nodoEmple;
+					System.out.println("Nombre: " + elem.getElementsByTagName("nombre").item(0).getTextContent());
+					System.out.println("Apellido: " + elem.getElementsByTagName("apellido").item(0).getTextContent());
+					System.out.println("Edad: " + elem.getElementsByTagName("edad").item(0).getTextContent());
+				}
+			}
+		} catch (Exception e) {
+		}
+	}
+	private static void ejercicio1() throws ParserConfigurationException, TransformerConfigurationException,
+			TransformerFactoryConfigurationError, TransformerException {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = factory.newDocumentBuilder();
 		DOMImplementation implementacion = builder.getDOMImplementation();
@@ -24,7 +53,6 @@ public class Principal {
 		Transformer transformer = TransformerFactory.newInstance().newTransformer();
 		transformer.transform(source, resultado);
 		System.out.println("End");
-
 	}
 	protected static void CrearAlumno(String nombre , String apellido, String edad,Document documento) {
 		Element padre = documento.createElement("alumno");
